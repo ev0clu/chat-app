e97198a36527c5194ca9718d3fdec7703441da3e
ev0clu <ev0clu@gmail.com>
2023-05-14 15:57:56 +0200

    Chat layout is updated

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,11 +1,25 @@
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import MessageWrapper from '../components/MessageWrapper';
+import InputWrapper from '../components/InputWrapper';
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: auto 1fr auto;
+`;
 
 const Chat = () => {
   return (
-    <Link to="/">
-      <div>Log out</div>
-    </Link>
+    <Wrapper>
+      <Sidebar />
+      <Header />
+      <MessageWrapper />
+      <InputWrapper />
+    </Wrapper>
   );
 };
 

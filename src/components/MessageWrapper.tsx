9d823f41ed158bb1b915ca408af83f7f16ab5f5b
ev0clu <ev0clu@gmail.com>
2023-05-18 11:23:06 +0200

    Message orientation set according to the logged user, component scrolling is added

--- src/components/MessageWrapper.tsx
+++ src/components/MessageWrapper.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import ThemeContext from './ThemeContext';
 import { useContext } from 'react';
+import { getAuth } from 'firebase/auth';
 
 interface WrapperProps {
   $themeColor: string;
@@ -10,7 +11,7 @@ const Wrapper = styled.div<WrapperProps>`
   grid-column: 2/3;
   grid-row: 2/3;
   display: flex;
-  flex-direction: row;
+  flex-direction: column-reverse;
   border-top: 1px solid
     ${(props) =>
       props.$themeColor === 'light' ? '#78716c' : '#d4d4d4'};
@@ -18,27 +19,40 @@ const Wrapper = styled.div<WrapperProps>`
     ${(props) =>
       props.$themeColor === 'light' ? '#78716c' : '#d4d4d4'};
   padding: 1rem;
+  overflow-y: auto;
+  scroll-behavior: smooth;
 `;
 
-const MessageWrapper = styled.div`
+interface MessageWrapperProps {
+  $orientation: string;
+}
+
+const MessageWrapper = styled.div<MessageWrapperProps>`
   display: flex;
   flex-direction: column;
   gap: 1rem;
   margin: 1rem;
+  align-items: ${(props) =>
+    props.$orientation === 'left' ? 'flex-start' : 'flex-end'};
 `;
 
 const StyledMessageBubble = styled.div`
   background: linear-gradient(-13deg, #106cce 49%, #2a4ab5 100%);
   color: #ffffff;
-  border-radius: 8px;
+  border-radius: 1.3rem;
   width: max-content;
-  padding: 0.5rem 1rem;
+  padding: 0.5rem 1.5rem;
   font-size: 1.1rem;
 `;
 
 const StyledName = styled.p`
   font-weight: 600;
   font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+`;
+
+const StyledText = styled.p`
+  word-break: break-all;
 `;
 
 interface Props {
@@ -59,14 +73,21 @@ const Messages = ({ message }: Props) => {
 
   return (
     <Wrapper $themeColor={theme === 'light' ? 'dark' : 'light'}>
-      <MessageWrapper>
-        {message.map((messageData) => (
-          <StyledMessageBubble key={messageData.id}>
+      {message.map((messageData) => (
+        <MessageWrapper
+          key={messageData.id}
+          $orientation={
+            messageData.uid === getAuth().currentUser?.uid
+              ? 'right'
+              : 'left'
+          }
+        >
+          <StyledMessageBubble>
             <StyledName>{messageData.name}</StyledName>
-            <p>{messageData.text}</p>
+            <StyledText>{messageData.text}</StyledText>
           </StyledMessageBubble>
-        ))}
-      </MessageWrapper>
+        </MessageWrapper>
+      ))}
     </Wrapper>
   );
 };
